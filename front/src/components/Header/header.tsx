f396ea6e54a35ca6d8ddc5382f25252d885a5f9f
Ilya <berside@bk.ru>
2024-11-15 20:56:05 +0300

    /css/

--- front/src/components/Header/header.tsx
+++ front/src/components/Header/header.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-const Header = ({ title, subtitle }) => {
+import './header.css';
+
+
+const Header = ({ title, subtitle, logoImage, profileText }) => {
   return (
     <header className="header">
-      <h1>{title}</h1>
-      {subtitle && <p className="subtitle">{subtitle}</p>}
+      <div className="logo-container">
+        <img src='../../imgs/Namix.png' alt='Логотип'/>
+      </div>
+      <div className="content-wrapper">
+        <h1>{title}</h1>
+        {subtitle && <p className="subtitle">{subtitle}</p>}
+      </div>
+      <div className="profile-container">
+        <span className="profile">Profile</span>
+      </div>
     </header>
   );
 };
+
 export default Header;
--- front/src/components/Home/Home.js
+++ front/src/components/Home/Home.js
@@ -1,12 +1,25 @@
 import { useNavigate } from 'react-router-dom';
+import GHeader from '../Header/header.tsx';
+import GFooter from '../Footer/footer.tsx'; 
 
 function Home() {
   const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    console.log('Button clicked');
+    navigate('/login');
+  };
+
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <button onClick={() => navigate('/login')}>Login</button>
-    </div>
+    <>
+      <GHeader />
+      <div>
+        <h1>О компании</h1>
+        <button onClick={handleLoginClick}>Login</button>
+      </div>
+      <GFooter />
+    </>
   );
 }
+
 export default Home;
