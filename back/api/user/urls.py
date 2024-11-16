551dd61d0c759db8192271def7f6bb94e42d0e2f
MonkaS <danchik646@gmail.com>
2024-11-16 18:08:03 +0300

    переделал юрлы и поменял время

--- back/api/user/urls.py
+++ back/api/user/urls.py
@@ -4,17 +4,13 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from .views import UserAuthViewSet, UserLoggedViewSet
 
 urlpatterns = [
-    # Swagger
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-
     # UserAuth
-    path('api/v1/auth/login/', UserAuthViewSet.as_view({'get': 'login'})),
-    path('api/v1/auth/register/', UserAuthViewSet.as_view({'post': 'register'})),
-    path('api/v1/auth/logout/', UserLoggedViewSet.as_view({'get': 'logout'})),
-    path('api/v1/auth/status/', UserLoggedViewSet.as_view({'get': 'status'})),
-    path('api/v1/user/get_one/', UserLoggedViewSet.as_view({'get': 'get_one'})),
-    path('api/v1/user/get_many/', UserLoggedViewSet.as_view({'get': 'get_many'})),
-    path('api/v1/user/update/', UserLoggedViewSet.as_view({'patch': 'update'})),
-    path('api/v1/user/delete/', UserLoggedViewSet.as_view({'delete': 'delete'})),
+    path('auth/login/', UserAuthViewSet.as_view({'get': 'login'})),
+    path('auth/register/', UserAuthViewSet.as_view({'post': 'register'})),
+    path('auth/logout/', UserLoggedViewSet.as_view({'get': 'logout'})),
+    path('auth/status/', UserLoggedViewSet.as_view({'get': 'status'})),
+    path('get_one/', UserLoggedViewSet.as_view({'get': 'get_one'})),
+    path('get_many/', UserLoggedViewSet.as_view({'get': 'get_many'})),
+    path('update/', UserLoggedViewSet.as_view({'patch': 'update'})),
+    path('delete/', UserLoggedViewSet.as_view({'delete': 'delete'})),
 ]
